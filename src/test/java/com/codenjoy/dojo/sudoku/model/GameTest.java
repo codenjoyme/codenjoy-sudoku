14008835cfef6b43e78f616a58f3eee717e0d2e3
Oleksandr_Baglai <apofig@gmail.com>
2021-02-23 14:47:51 +0200

    [games] Refactoring: Renname main game logic test (SampleTest -> SingleplayerTest, SingleTest -> MultiplayerTest)

--- src/test/java/com/codenjoy/dojo/sudoku/model/GameTest.java
+++ src/test/java/com/codenjoy/dojo/sudoku/model/GameTest.java
@@ -37,7 +37,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-public class SudokuTest {
+public class GameTest {
 
     public static final String INITIAL =
             "☼☼☼☼☼☼☼☼☼☼☼☼☼" +
